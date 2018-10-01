0dcc8f786b14cdd5895d41fe729eddc0b19c5deb
RyujiTamaki <e44119rt@gmail.com>
2018-10-02 01:44:43 +0900

    add session example

--- guestbook.py
+++ guestbook.py
@@ -17,12 +17,13 @@
 # [START imports]
 import os
 import urllib
+import hashlib
 
-from google.appengine.api import users
 from google.appengine.ext import ndb
 
 import jinja2
 import webapp2
+from webapp2_extras import sessions
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -50,7 +51,6 @@ def guestbook_key(guestbook_name=DEFAULT_GUESTBOOK_NAME):
 class Author(ndb.Model):
     """Sub model for representing an author."""
     identity = ndb.StringProperty(indexed=False)
-    email = ndb.StringProperty(indexed=False)
 
 
 class Greeting(ndb.Model):
@@ -61,8 +61,27 @@ class Greeting(ndb.Model):
 # [END greeting]
 
 
+class User(ndb.Model):
+    user_id = ndb.StringProperty(indexed=False)
+    password = ndb.StringProperty(indexed=False)
+
+
+class BaseHandler(webapp2.RequestHandler):
+    def dispatch(self):
+        self.session_store = sessions.get_store(request=self.request)
+
+        try:
+            webapp2.RequestHandler.dispatch(self)
+        finally:
+            self.session_store.save_sessions(self.response)
+
+    @webapp2.cached_property
+    def session(self):
+        return self.session_store.get_session()
+
+
 # [START main_page]
-class MainPage(webapp2.RequestHandler):
+class MainPage(BaseHandler):
 
     def get(self):
         guestbook_name = self.request.get('guestbook_name',
@@ -71,29 +90,25 @@ class MainPage(webapp2.RequestHandler):
             ancestor=guestbook_key(guestbook_name)).order(-Greeting.date)
         greetings = greetings_query.fetch(10)
 
-        user = users.get_current_user()
-        if user:
-            url = users.create_logout_url(self.request.uri)
-            url_linktext = 'Logout'
+        if self.session.get('user_id'):
+            template_values = {
+                'user': self.session['user_id'],
+                'greetings': greetings,
+                'guestbook_name': urllib.quote_plus(guestbook_name),
+            }
+            template = JINJA_ENVIRONMENT.get_template('index.html')
+            self.response.write(template.render(template_values))
         else:
-            url = users.create_login_url(self.request.uri)
-            url_linktext = 'Login'
-
-        template_values = {
-            'user': user,
-            'greetings': greetings,
-            'guestbook_name': urllib.quote_plus(guestbook_name),
-            'url': url,
-            'url_linktext': url_linktext,
-        }
-
-        template = JINJA_ENVIRONMENT.get_template('index.html')
-        self.response.write(template.render(template_values))
+            template_values = {
+                'error_message': 'no session',
+            }
+            template = JINJA_ENVIRONMENT.get_template('login.html')
+            self.response.write(template.render(template_values))
 # [END main_page]
 
 
 # [START guestbook]
-class Guestbook(webapp2.RequestHandler):
+class Guestbook(BaseHandler):
 
     def post(self):
         # We set the same parent key on the 'Greeting' to ensure each
@@ -105,10 +120,8 @@ class Guestbook(webapp2.RequestHandler):
                                           DEFAULT_GUESTBOOK_NAME)
         greeting = Greeting(parent=guestbook_key(guestbook_name))
 
-        if users.get_current_user():
-            greeting.author = Author(
-                    identity=users.get_current_user().user_id(),
-                    email=users.get_current_user().email())
+        if self.session.get('user_id'):
+            greeting.author = Author(identity=self.session.get('user_id'))
 
         greeting.content = self.request.get('content')
         greeting.put()
@@ -118,9 +131,55 @@ class Guestbook(webapp2.RequestHandler):
 # [END guestbook]
 
 
+class Login(BaseHandler):
+
+    def post(self):
+        user_id = self.request.get('user_id')
+        password = hashlib.sha256(self.request.get('password')).hexdigest()
+
+        @ndb.transactional
+        def insert_if_absent(user_key, user):
+            fetch = user_key.get()
+            if fetch is None:
+                user.put()
+                return True
+            return False
+
+        user_key = ndb.Key(User, user_id)
+        user = User(id=user_id, user_id=user_id, password=password)
+        inserted = insert_if_absent(user_key, user)
+
+        if not inserted:
+            user = user_key.get()
+
+        if user.user_id == user_id and user.password == password:
+            self.session['user_id'] = user_id
+            self.redirect('/')
+        else:
+            template_values = {
+                'error_message': 'login failed',
+            }
+            template = JINJA_ENVIRONMENT.get_template('login.html')
+            self.response.write(template.render(template_values))
+
+
+class Logout(BaseHandler):
+
+    def get(self):
+        self.session['user_id'] = None
+        self.redirect('/')
+
+
 # [START app]
+config = {}
+config['webapp2_extras.sessions'] = {
+    'secret_key': 'some-secret-key',
+}
+
 app = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/sign', Guestbook),
-], debug=True)
+    ('/login', Login),
+    ('/logout', Logout)
+], debug=True, config=config)
 # [END app]
